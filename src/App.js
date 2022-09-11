3e15d2aef8839a3df5b288dcb81226ab28b9f0b0
GarryAmasia <garryamasia@outlook.com>
2022-09-11 18:20:00 +1000

    adding dynamic data from form to expenseItem component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 500,
+    date: new Date(2022, 9, 5),
+  },
+  {
+    id: "e2",
+    title: "Mortgage",
+    amount: 400,
+    date: new Date(2022, 8, 4),
+  },
+  {
+    id: "e3",
+    title: "Fishing stuff",
+    amount: 299,
+    date: new Date(2022, 7, 4),
+  },
+  {
+    id: "e4",
+    title: "Learning",
+    amount: 299,
+    date: new Date(2022, 6, 2),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 500,
-      date: new Date(2022, 9, 5),
-    },
-    {
-      id: "e2",
-      title: "Mortgage",
-      amount: 400,
-      date: new Date(2022, 8, 4),
-    },
-    {
-      id: "e3",
-      title: "Fishing stuff",
-      amount: 299,
-      date: new Date(2022, 7, 4),
-    },
-    {
-      id: "e4",
-      title: "Learning",
-      amount: 299,
-      date: new Date(2022, 6, 2),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("in app js");
-    console.log(expense);
+    // setExpenses([expense, ...expenses]);
+    setExpenses((prevState) => {
+      return [expense, ...prevState];
+    });
   };
 
   return (
--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -2,24 +2,15 @@ import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import { useState } from "react";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("updated");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
-      <button onClick={clickHandler}>click me</button>
     </Card>
   );
 };
